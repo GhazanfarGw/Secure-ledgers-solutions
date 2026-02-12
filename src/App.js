168db8fc9d76d259caf1ac6cdc50197f81b3afae
Ghazanfar Abbas <ghazanfargwi@gmail.com>
2026-02-13 02:57:04 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import { Routes, Route } from "react-router-dom";
 import React, { useEffect, useState } from 'react';
 import Home from "./Home/index";
-// import About from "./Aboutus/Index";
-// import Service from "./Services/index";
-// import Consulting from "./Cryptocul/Index"
+import About from "./Aboutus/Index";
+import Service from "./Services/index";
+import Consulting from "./Cryptocul/Index"
 // import Contact from "./Home/Contact";
 import Preloader from "./Home/Preload";
-// import Online from "./Online Payment/Index"
-// import Billing from "./Billing/Index"
-// import Retails from "./Retails/Index"
-// import Payroll from "./payroll/Index"
-// import Liquidity from "./Liquidity Management/Index"
-// import Security from "./Security Risk/Index"
-// import Assets from "./Asset Protection/Index"
-// import Stuck from "./Funds Stuck/Index"
-// import Client from "./Client Experience/Index"
+import Online from "./Online Payment/Index"
+import Billing from "./Billing/Index"
+import Retails from "./Retails/Index"
+import Payroll from "./payroll/Index"
+import Liquidity from "./Liquidity Management/Index"
+import Security from "./Security Risk/Index"
+import Assets from "./Asset Protection/Index"
+import Stuck from "./Funds Stuck/Index"
+import Client from "./Client Experience/Index"
 
 function App() {
   const [loading, setLoading] = useState(true);
@@ -35,9 +35,9 @@ function App() {
       ) : (
         <Routes>
           <Route path="/" element={ <Home/> } />
-          {/* <Route path="/about-us" element={ <About/> } />
+          <Route path="/about-us" element={ <About/> } />
           <Route path="/services" element={ <Service/> } />
-          <Route path="/contact-us" element={ <Contact/> } />
+          {/* <Route path="/contact-us" element={ <Contact/> } /> */}
           <Route path="/online-consulting" element={ <Consulting/> } />
           <Route path="/online-payments" element={ <Online/> } />
           <Route path="/billing" element={ <Billing/> } />
@@ -47,7 +47,7 @@ function App() {
           <Route path="/security-risk" element={ <Security/> } />
           <Route path="/asset-protection" element={ <Assets/> } />
           <Route path="/funds-stuck" element={ <Stuck/> } />
-          <Route path="/client-experience" element={ <Client/> } /> */}
+          <Route path="/client-experience" element={ <Client/> } />
         </Routes>
       )}
     </>
