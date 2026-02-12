91baaa3cacb5c28d6f73d753b7118191edf3a828
Ghazanfar Abbas <ghazanfargwi@gmail.com>
2026-02-13 03:00:32 +0500

    Add About component to Online Payment Index

--- src/Online Payment/Index.jsx
+++ src/Online Payment/Index.jsx
@@ -3,6 +3,7 @@ import Dashboard from "./Dashboard"
 import Price from "./Price"
 import Anywhere from "./Anywhere"
 import Subr from "./Subr"
+import About from "./About"
 import Slider from "./Slider"
 import Header from "../Home/header"
 import Footer from "../Home/Footer"
@@ -21,4 +22,3 @@ export default function Index() {
     </>
   )
 }
-
